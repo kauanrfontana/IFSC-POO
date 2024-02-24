0ae4d165e002be2fe65059d05d8594ff6a606185
Kauan Ramos Fontana <kauanrfontana@gmail.com>
2024-02-23 22:14:26 -0300

    adicionado .gitignore

---
+++ revisao/Arranjos.java
@@ -0,0 +1,10 @@
+package revisao;
+import java.util.Arrays;
+
+public class Arranjos {
+    public static void main(String[] args){
+        int[] array = {1, 2};
+
+        System.out.println(Arrays.toString(array));
+    }
+}
--- revisao/OlaMundo.java
+++ revisao/OlaMundo.java
@@ -1,3 +1,4 @@
+package revisao;
 // OlaMundo.java
 public class OlaMundo{
 
